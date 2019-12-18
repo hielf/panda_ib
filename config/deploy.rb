21f4486afd8b700bc494f25993c81f8ee43405dd
moumou <hielf@qq.com>
2019-12-18 21:34:10 +0800

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,8 +4,8 @@ lock "~> 3.11.0"
 set :repo_url, "git@github.com:hielf/panda_ib.git"
 set :application, "panda_ib"
 # set :user, "deploy"
-set :puma_threads, [2, 8]
-set :puma_workers, 2
+set :puma_threads, [1, 1]
+set :puma_workers, 1
 set :rails_env, 'production'
 # set :rvm_ruby_version, '2.4.0@panda_ib'
 
