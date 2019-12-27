97779db9a95aef971cc1ef17cee605e8c1c8fc3c
moumou <hielf@qq.com>
2019-12-27 09:05:27 +0800

    test

--- app/jobs/traders_job.rb
+++ app/jobs/traders_job.rb
@@ -10,19 +10,19 @@ class TradersJob < ApplicationJob
   def perform(*args)
     contract = args[0]
     Rails.logger.warn "ib start: #{contract}, #{Time.zone.now}"
-    # market_data = ApplicationController.helpers.market_data(contract)
-    # if market_data
-    #   file = ApplicationController.helpers.index_to_csv(contract)
-    #   data = ApplicationController.helpers.online_data(file)
-    #   if data
-    #     current_time = Time.zone.now.strftime('%H:%M')
-    #     if (current_time > "09:15" && current_time < "12:00") || (current_time > "13:00" && current_time < "15:30")
-    #       ApplicationController.helpers.check_position(data)
-    #     else
-    #       ApplicationController.helpers.close_position
-    #     end
-    #   end
-    # end
+    market_data = ApplicationController.helpers.market_data(contract)
+    if market_data
+      file = ApplicationController.helpers.index_to_csv(contract)
+      data = ApplicationController.helpers.online_data(file)
+      if data
+        current_time = Time.zone.now.strftime('%H:%M')
+        if (current_time > "09:15" && current_time < "12:00") || (current_time > "13:00" && current_time < "15:30")
+          ApplicationController.helpers.check_position(data)
+        else
+          ApplicationController.helpers.close_position
+        end
+      end
+    end
   end
 
   private
--- lib/tasks/test.rake
+++ lib/tasks/test.rake
@@ -2,11 +2,11 @@ namespace :ib do
   task :test => :environment do
 
     Rails.logger.warn "ib test: start"
-    # url = "http://127.0.0.1/api/trade_orders/test"
-    # res = HTTParty.get(url)
+    url = "http://127.0.0.1/api/trade_orders/test"
+    res = HTTParty.get(url)
 
-    contract = "hsi"
-    TradersJob.perform_now contract
+    # contract = "hsi"
+    # TradersJob.perform_now contract
 
     Rails.logger.warn "ib test: end"
 
