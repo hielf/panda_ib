d4bfbaea5f78c8dab116a7682184517da4201421
moumou <hielf@qq.com>
2020-04-21 01:04:49 +0800

    Update clock.rb

--- lib/job/clock.rb
+++ lib/job/clock.rb
@@ -1,9 +1,18 @@
 require 'clockwork'
+require 'clockwork/database_events'
 require '../../config/boot'
 require '../../config/environment'
 require 'active_support/time' # Allow numeric durations (eg: 1.minutes)
 
 module Clockwork
+  # configure do |config|
+  #   config[:sleep_timeout] = 5
+  #   config[:logger] = Logger.new(log_file_path)
+  #   config[:tz] = 'EST'
+  #   config[:max_threads] = 15
+  #   config[:thread] = true
+  # end
+
   handler do |job|
     puts "Running job"
   end
@@ -13,7 +22,7 @@ module Clockwork
   #   puts "Running #{job}, at #{time}"
   # end
   # every(1.second, (TradersJob.perform_now 'hsi'), :if => lambda { |t| t.sec == 0 })
-  every(1.minute, (Rails.logger.warn "clock 1min start: #{Time.now.to_s}"))
+  every(1.minute, (Rails.logger.warn "clock 1min start: #{Time.now.to_s}"), :skip_first_run => true)
   # every(1.hour, 'hourly.job')
   #
   # every(1.day, 'midnight.job', :at => '00:00')
