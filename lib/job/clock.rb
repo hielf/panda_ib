1ebd4334e3e7d2c84f9d705086c1790c3ae25e95
moumou <hielf@qq.com>
2020-04-23 23:38:57 +0800

    Update clock.rb

--- lib/job/clock.rb
+++ lib/job/clock.rb
@@ -4,31 +4,26 @@ require '../../config/boot'
 require '../../config/environment'
 require 'active_support/time' # Allow numeric durations (eg: 1.minutes)
 
-every(1.minute, (Rails.logger.warn "clock 1min start: #{Time.now.to_s}"), :skip_first_run => true, :thread => true)
+module Clockwork
+  # configure do |config|
+  #   config[:sleep_timeout] = 5
+  #   config[:logger] = Logger.new(log_file_path)
+  #   config[:tz] = 'EST'
+  #   config[:max_threads] = 15
+  #   config[:thread] = true
+  # end
 
-# module Clockwork
-#   # configure do |config|
-#   #   config[:sleep_timeout] = 5
-#   #   config[:logger] = Logger.new(log_file_path)
-#   #   config[:tz] = 'EST'
-#   #   config[:max_threads] = 15
-#   #   config[:thread] = true
-#   # end
-#
-#   handler do |job|
-#     puts "Running job"
-#   end
-#
-#   # handler receives the time when job is prepared to run in the 2nd argument
-#   # handler do |job, time|
-#   #   puts "Running #{job}, at #{time}"
-#   # end
-#   # every(1.second, (TradersJob.perform_now 'hsi'), :if => lambda { |t| t.sec == 0 })
-#
-#   # every(1.hour, 'hourly.job')
-#   #
-#   # every(1.day, 'midnight.job', :at => '00:00')
-# end
+  # handler receives the time when job is prepared to run in the 2nd argument
+  handler do |job, time|
+    puts "Running #{job}, at #{time}"
+    Rails.logger.warn "clock 1min start: #{Time.now.to_s}"
+  end
+  # every(1.second, (TradersJob.perform_now 'hsi'), :if => lambda { |t| t.sec == 0 })
+  every(1.minute, 'timing', :skip_first_run => true, :thread => true)
+  # every(1.hour, 'hourly.job')
+  #
+  # every(1.day, 'midnight.job', :at => '00:00')
+end
 
 # cd /var/www/panda_ib/current/lib/job && clockworkd -c clock.rb start --log -d /var/www/panda_ib/current/lib/job
 # clockworkd -c clock.rb start --log -d /Users/hielf/workspace/projects/panda_ib/lib/job
