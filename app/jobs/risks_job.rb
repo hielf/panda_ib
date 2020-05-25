8281513707367d54db243d3b4e5289fca3a91996
moumou <hielf@qq.com>
2020-05-25 14:25:37 +0800

    Update risks_job.rb

--- app/jobs/risks_job.rb
+++ app/jobs/risks_job.rb
@@ -13,35 +13,26 @@ class RisksJob < ApplicationJob
     Rails.logger.warn "ib risk start: #{contract}, #{Time.zone.now}"
     @ib = ApplicationController.helpers.ib_connect
     if @ib.isConnected()
+      loss_limit = ENV["total_asset"].to_f * 0.001 * -1
       market_data = ApplicationController.helpers.market_data(contract, true)
+      trades = ApplicationController.helpers.ib_trades
+      last_trade = trades.sort_by { |h| -h[:time] }.reverse.last
       close = market_data.iloc[-1].close
-      last_trade = Trade.last
       unrealizedPNL = 0
       if last_trade.realized_pnl == 0
-        case last_trade.action
+        case last_trade[:action]
         when "BUY"
-          unrealizedPNL = (close - last_trade.price) * ENV['amount'].to_i * 50
+          unrealizedPNL = (close - last_trade[:price]) * ENV['amount'].to_i * 50
         when "SELL"
-          unrealizedPNL = -1 * (close - last_trade.price) * ENV['amount'].to_i * 50
+          unrealizedPNL = -1 * (close - last_trade[:price]) * ENV['amount'].to_i * 50
         end
       end
-      Rails.logger.warn "ib risk position: #{last_trade.action}, open: #{last_trade.price}, close: #{close}, current unrealizedPNL: #{unrealizedPNL}, #{Time.zone.now}" if unrealizedPNL != 0
-      loss_limit = ENV["total_asset"].to_f * 0.001 * -1
-      if unrealizedPNL < loss_limit
+      Rails.logger.warn "ib risk position: #{last_trade[:action]}, open: #{last_trade[:price]}, close: #{close}, current unrealizedPNL: #{unrealizedPNL}, #{Time.zone.now}" if unrealizedPNL != 0
+
+      if unrealizedPNL < 0 && unrealizedPNL < loss_limit
         Rails.logger.warn "ib risk loss_limit: #{loss_limit} alert: #{unrealizedPNL}, #{Time.zone.now}"
         ApplicationController.helpers.close_position
       end
-      #
-      #
-      #
-      # data = ApplicationController.helpers.ib_portfolio
-      # Rails.logger.warn "ib risk close: #{close}, current unrealizedPNL: #{data["unrealizedPNL"]}, #{Time.zone.now}" if !data["unrealizedPNL"].nil?
-      # loss_limit = ENV["total_asset"].to_f * 0.001 * -1
-      #
-      # if !data["unrealizedPNL"].nil? && data["unrealizedPNL"] < loss_limit
-      #   Rails.logger.warn "ib risk loss_limit alert: #{data["unrealizedPNL"]}, #{Time.zone.now}"
-      #   ApplicationController.helpers.close_position
-      # end
     end
   end
 
