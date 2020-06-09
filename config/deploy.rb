bbcae29bfc6cc855f6a5542d44b2525d8f47dbd8
moumou <hielf@qq.com>
2020-06-09 23:23:43 +0800

    capistrano god

--- config/deploy.rb
+++ config/deploy.rb
@@ -95,7 +95,8 @@ namespace :deploy do
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
-      invoke 'puma:restart'
+      # invoke 'puma:restart'
+      invoke 'god:deploy'
     end
   end
 
