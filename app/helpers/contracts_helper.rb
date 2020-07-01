c0a4caff5b190e05cb9243ab67269ff0d8610d03
moumou <hielf@qq.com>
2020-07-01 17:04:34 +0800

    begin time adjust

--- app/helpers/contracts_helper.rb
+++ app/helpers/contracts_helper.rb
@@ -214,7 +214,8 @@ module ContractsHelper
     # end_date = 1.business_day.from_now.to_date
     skip_minute = Time.zone.now < (Time.parse "10:45 am") ? 75 : 0
     #回溯1小时，舍去9:15-9:44 & 15:45-16:29交易时间
-    begin_time = CSV.read(csv)[CSV.read(csv).count-skip_minute-90][0].to_time
+    # begin_time = CSV.read(csv)[CSV.read(csv).count-skip_minute-90][0].to_time
+    begin_time = 10.days.before(end_time)
     end_time = (Time.zone.now + 1.minute).to_time
     system( "cd #{Rails.root.to_s + '/lib/python/ib'} && python3 v4_#{ENV["backtrader_version"]}.py '#{csv}' '#{json}' '#{begin_time}' '#{end_time}'" )
     data = JSON.parse(File.read(json))
