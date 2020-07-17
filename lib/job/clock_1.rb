9476aaf55dce6b7ae15ee16329c0c709dbdf3f53
moumou <hielf@qq.com>
2020-07-17 11:27:39 +0800

    Update clock_1.rb

--- lib/job/clock_1.rb
+++ lib/job/clock_1.rb
@@ -26,7 +26,7 @@ module Clockwork
     TradersJob.perform_now contract if job == 'ib.trader'
   end
 
-  every(1.second, 'ib.trader', :if => lambda {  |t| [11,41].include? t.sec }, :thread => true) if ENV["backtrader_version"] == "15sec"
+  every(1.second, 'ib.trader', :if => lambda {  |t| [11,26,41,56].include? t.sec }, :thread => true) if ENV["backtrader_version"] == "15sec"
   every(1.second, 'ib.trader', :if => lambda { |t| (([0,3,6,9,12,15,18,21,24,27,30,33,36,39,42,45,48,51,54,57].include? t.min) && t.sec == 56) }, :thread => true) if ENV["backtrader_version"] == "3min"
   # every(1.second, 'ib.trader', :if => lambda {  |t| [0,15,30,45].include? t.sec }, :thread => true) if ENV["backtrader_version"] == "3min"
   every(1.second, 'ib.trader', :if => lambda { |t| t.sec == 56 }, :thread => true) if ENV["backtrader_version"] == "4min"
