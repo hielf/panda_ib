8cac40a99c11ad2a444611f8e3e3f4173a269d48
moumou <hielf@qq.com>
2021-03-09 20:35:07 +0800

    Update trader_position.rb

--- app/models/trader_position.rb
+++ app/models/trader_position.rb
@@ -1,6 +1,6 @@
 class TraderPosition < ApplicationRecord
-
-  def init(contract)
+# TraderPosition.init(contract)
+  def self.init(contract)
     tp = TraderPosition.find_or_initialize_by(contract: contract)
     if tp.position.nil?
       tp.position = 0
