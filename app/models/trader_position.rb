6d333596aeff31706ada659487a7b45233cbcccb
moumou <hielf@qq.com>
2021-03-09 20:29:21 +0800

    Update trader_position.rb

--- app/models/trader_position.rb
+++ app/models/trader_position.rb
@@ -1,2 +1,11 @@
 class TraderPosition < ApplicationRecord
+
+  def init(contract)
+    tp = TraderPosition.find_or_initialize_by(contract: contract)
+    if tp.position.nil?
+      tp.position = 0
+      tp.save!
+    end
+    return tp
+  end
 end
